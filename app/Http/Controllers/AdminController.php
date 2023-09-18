4d9d04e76852f584d53d658a67799e4f396d81ee
Andrea Torres <andrea.torres8087@alumnos.udg.mx>
2023-09-18 16:45:25 -0600

    Actualizacion creates

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin/indexAdmin');
     }
 
     /**
@@ -20,7 +20,7 @@ class AdminController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin/createadmin');
     }
 
     /**
