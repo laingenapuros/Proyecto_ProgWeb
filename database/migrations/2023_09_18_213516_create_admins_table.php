5775ce5eca76e3f54f5fba82769fd3b2a74e5f56
Andrea Torres <andrea.torres8087@alumnos.udg.mx>
2023-09-18 16:16:46 -0600

    Creacion DB

--- database/migrations/2023_09_18_213516_create_admins_table.php
+++ database/migrations/2023_09_18_213516_create_admins_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table -> string('nombre');
+            $table -> string('correo');
+            $table -> string('password');
+
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('Proveedor', ProveedorController::class); //agregar controladores 
+Route::resource('Empleado', EmpleadoController::class); //agregar controladores 
+Route::resource('Admin', AdminController::class); //agregar controladores 
+Route::resource('Producto', ProductoController::class); //agregar controladores 
+Route::resource('Cliente', ClienteController::class); //agregar controladores 
