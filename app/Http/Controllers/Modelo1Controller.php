c170c3ec17cb6bad489313ae9d52b9c57fe2d95a
Andrea Torres <andrea.torres8087@alumnos.udg.mx>
2023-09-18 15:38:16 -0600

    Creacion tablas

---
+++ app/Http/Controllers/Modelo1Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Modelo1;
+use Illuminate\Http\Request;
+
+class Modelo1Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Modelo1 $modelo1)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Modelo1 $modelo1)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Modelo1 $modelo1)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Modelo1 $modelo1)
+    {
+        //
+    }
+}
