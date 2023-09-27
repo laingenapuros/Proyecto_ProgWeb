983f0ac3a7239e339255edcc953a083bc2ace76b
Andrea Torres <andrea.torres8087@alumnos.udg.mx>
2023-09-27 16:20:42 -0600

    Admin complete

--- resources/views/admin/indexAdmin.blade.php
+++ resources/views/admin/indexAdmin.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+</html><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,9 +10,21 @@
     <ul>
         
     @foreach ($admin as $admin)
-    <li> {{$admin ->nombre}} </li> 
+    <a href="{{route('admin.show', $admin)}}">
+        {{$admin -> nombre}}
+    </a>
+    <a href="{{route('admin.edit', $admin)}}">
+          Editar
+    </a>
+    <form action ="{{route('admin.destroy',$admin)}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit"> eliminar </button>
+</form> 
+
+    <!--<li> {{$admin ->nombre}} </li> 
     <li> {{$admin ->correo}} </li>
-    <li> {{$admin ->password}} </li>
+    <li> {{$admin ->password}} </li>-->
     @endforeach
 </ul>
 </body>
