0df7e88b3f748b637473b6af767665ba0ac387a2
Andrea Torres <andrea.torres8087@alumnos.udg.mx>
2023-10-15 22:50:04 -0600

    Plantilla agregada a CRUD admin y empleado

---
+++ app/Http/Controllers/ComprasController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Compras;
+use Illuminate\Http\Request;
+
+class ComprasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource. 
+     */
+    public function create()
+    {
+        //
+        $compras = Compra::all();
+        return view('req.createComp', compact());
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Compras $compras)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Compras $compras)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Compras $compras)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Compras $compras)
+    {
+        //
+    }
+}
